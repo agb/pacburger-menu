cf6fa9c76f4b0b4c6539b541215561992b3ba778
Ali Bektash <agokselb@gmail.com>
2024-11-06 13:06:28 +0900

    feat: :sparkles: Deleted prototype file and included DOM and ES2018 library
    Added es2018 and dom library because needed for import(for using await promise func) style.css file

--- pacburger-menu.js
+++ pacburger-menu.js
@@ -1,6 +1,42 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var PacBurgerMenu = /** @class */ (function () {
-    function PacBurgerMenu(menuItems) {
-        this.menuItems = this._createMenuItems(menuItems);
+    function PacBurgerMenu(rawMenuItems) {
+        this.menuItems = this._createMenuItems(rawMenuItems);
         this._createPacBurger();
         this._addStyles();
         this._addToggleEvent();
@@ -17,7 +53,7 @@ var PacBurgerMenu = /** @class */ (function () {
     };
     PacBurgerMenu.prototype._createPacBurger = function () {
         var pacburgerDiv = document.createElement("div");
-        pacburgerDiv.className = "pacburger";
+        pacburgerDiv.className = "__pacburger";
         var nav = document.createElement("nav");
         nav.className = "active";
         nav.id = "nav";
@@ -43,10 +79,23 @@ var PacBurgerMenu = /** @class */ (function () {
         document.body.appendChild(pacburgerDiv);
     };
     PacBurgerMenu.prototype._addStyles = function () {
-        var styles = ".icon,.pacburger{justify-content:center}#_pacburger__circle,#_pacburger__triangle,.circle{width:1.5rem;height:1.5rem}body,nav ul,ul{margin:0}nav,nav ul{transition:width 1s linear}a,nav ul a{color:#000;text-decoration:none}.icon,nav ul,ul{padding:0}.circle,.icon,nav ul a{position:relative}.pacburger{display:flex}.active{display:block}.circle,.icon,body,nav,nav ul{display:flex}#nav{background-color:#fff;padding:1rem;border-radius:.5rem}ul{list-style:none}li{margin:.5rem 0}.icon{align-items:center;background:0 0}.circle{background-color:#fbbf24;border-radius:50%;justify-content:center;align-items:center}#_pacburger__circle{background-color:#fcd34d;border-radius:50%;position:absolute}#_pacburger__triangle,.icon,nav{background-color:#fff}#_pacburger__triangle{transform:rotate(45deg);margin-left:-2rem;position:absolute;z-index:10}@keyframes growShrink{0%,100%{height:0}50%{height:24px}}body{font-family:Muli,sans-serif;align-items:center;justify-content:center;height:100vh}nav{padding:20px;width:80px;align-items:center;justify-content:center;border-radius:3px;box-shadow:0 2px 5px rgba(0,0,0,.3);overflow-x:hidden}nav.active{width:350px}nav ul{list-style-type:none;width:0}nav.active ul{width:100%}nav ul li{transform:rotateY(0);opacity:0;transition:transform 2s linear,opacity 1s linear}nav.active ul li{opacity:1;transform:rotateY(360deg)}nav ul a{margin:0 10px}.icon{border:0;cursor:pointer;height:30px;width:30px}.icon:focus{outline:0}.icon .line{background-color:#000;height:2px;width:20px;position:absolute;top:10px;left:5px;transition:transform 2s linear}.icon .line2{top:auto;bottom:10px}nav.active .icon .line1{transform:rotate(-765deg) translateY(5.5px)}nav.active .icon .line2{transform:rotate(765deg) translateY(-5.5px)}nav:not(.active) #_pacburger__triangle{animation:.8s infinite growShrink}";
-        var styleSheet = document.createElement("style");
-        styleSheet.innerText = styles;
-        document.head.appendChild(styleSheet);
+        return __awaiter(this, void 0, void 0, function () {
+            var response, styles, styleSheet;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, fetch("/style.css")];
+                    case 1:
+                        response = _a.sent();
+                        return [4 /*yield*/, response.text()];
+                    case 2:
+                        styles = _a.sent();
+                        styleSheet = document.createElement("style");
+                        styleSheet.innerText = styles;
+                        document.head.appendChild(styleSheet);
+                        return [2 /*return*/];
+                }
+            });
+        });
     };
     PacBurgerMenu.prototype._addToggleEvent = function () {
         var toggle = document.getElementById("toggle");
--- pacburger-menu.ts
+++ pacburger-menu.ts
@@ -1,16 +1,19 @@
+interface rawMenuList {
+  label: string;
+  href: string;
+}
+
 class PacBurgerMenu {
   private menuItems: HTMLLIElement[];
 
-  constructor(menuItems: { label: string; href: string }[]) {
-    this.menuItems = this._createMenuItems(menuItems);
+  constructor(rawMenuItems: rawMenuList[]) {
+    this.menuItems = this._createMenuItems(rawMenuItems);
     this._createPacBurger();
     this._addStyles();
     this._addToggleEvent();
   }
 
-  private _createMenuItems(
-    items: { label: string; href: string }[]
-  ): HTMLLIElement[] {
+  private _createMenuItems(items: rawMenuList[]): HTMLLIElement[] {
     return items.map((item) => {
       const li = document.createElement("li") as HTMLLIElement;
       const a = document.createElement("a");
@@ -23,7 +26,7 @@ class PacBurgerMenu {
 
   private _createPacBurger(): void {
     const pacburgerDiv = document.createElement("div");
-    pacburgerDiv.className = "pacburger";
+    pacburgerDiv.className = "__pacburger";
 
     const nav = document.createElement("nav");
     nav.className = "active";
@@ -59,11 +62,12 @@ class PacBurgerMenu {
     document.body.appendChild(pacburgerDiv);
   }
 
-  private _addStyles(): void {
-    const styles = `.icon,.pacburger{justify-content:center}#_pacburger__circle,#_pacburger__triangle,.circle{width:1.5rem;height:1.5rem}body,nav ul,ul{margin:0}nav,nav ul{transition:width 1s linear}a,nav ul a{color:#000;text-decoration:none}.icon,nav ul,ul{padding:0}.circle,.icon,nav ul a{position:relative}.pacburger{display:flex}.active{display:block}.circle,.icon,body,nav,nav ul{display:flex}#nav{background-color:#fff;padding:1rem;border-radius:.5rem}ul{list-style:none}li{margin:.5rem 0}.icon{align-items:center;background:0 0}.circle{background-color:#fbbf24;border-radius:50%;justify-content:center;align-items:center}#_pacburger__circle{background-color:#fcd34d;border-radius:50%;position:absolute}#_pacburger__triangle,.icon,nav{background-color:#fff}#_pacburger__triangle{transform:rotate(45deg);margin-left:-2rem;position:absolute;z-index:10}@keyframes growShrink{0%,100%{height:0}50%{height:24px}}body{font-family:Muli,sans-serif;align-items:center;justify-content:center;height:100vh}nav{padding:20px;width:80px;align-items:center;justify-content:center;border-radius:3px;box-shadow:0 2px 5px rgba(0,0,0,.3);overflow-x:hidden}nav.active{width:350px}nav ul{list-style-type:none;width:0}nav.active ul{width:100%}nav ul li{transform:rotateY(0);opacity:0;transition:transform 2s linear,opacity 1s linear}nav.active ul li{opacity:1;transform:rotateY(360deg)}nav ul a{margin:0 10px}.icon{border:0;cursor:pointer;height:30px;width:30px}.icon:focus{outline:0}.icon .line{background-color:#000;height:2px;width:20px;position:absolute;top:10px;left:5px;transition:transform 2s linear}.icon .line2{top:auto;bottom:10px}nav.active .icon .line1{transform:rotate(-765deg) translateY(5.5px)}nav.active .icon .line2{transform:rotate(765deg) translateY(-5.5px)}nav:not(.active) #_pacburger__triangle{animation:.8s infinite growShrink}`;
-
+  private async _addStyles(): Promise<void> {
+    const response = await fetch("/style.css");
+    const styles = await response.text();
     const styleSheet = document.createElement("style");
     styleSheet.innerText = styles;
+
     document.head.appendChild(styleSheet);
   }
 
@@ -79,7 +83,7 @@ class PacBurgerMenu {
   }
 }
 
-const pacburgerList = [
+const pacburgerList: rawMenuList[] = [
   { label: "#lgsta", href: "#" },
   { label: "#2", href: "#" },
   { label: "#3", href: "#" },
